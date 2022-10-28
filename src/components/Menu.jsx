85a5db39ffccbee6d63a0f8d5e7ece506a07d2bd
chegarty74 <chegarty74@yahoo.com>
2022-10-28 18:54:38 -0500

    Add initial header and nav styling.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,33 +6,41 @@ import { BsSearch } from "react-icons/bs";
 function Menu() {
 	return (
 		<>
-			<nav>
-				<div>
-					<picture>
-						<img src="" alt="" />
-					</picture>
-				</div>
-				<div>
-					<ul>
-						<li>
-							<NavLink to="/traveljobs">Travel Jobs</NavLink>
-						</li>
-						<li>
-							<NavLink to="/benefits">Benefits</NavLink>
-						</li>
-						<li>
-							<NavLink to="/about">About</NavLink>
-						</li>
-						<li>
-							<NavLink to="/blogpage">Blog</NavLink>
-						</li>
-						<li>
-							<NavLink>{BsSearch}</NavLink>
-						</li>
-					</ul>
-				</div>
-				<div>{FaUserCircle}</div>
-			</nav>
+			<header>
+				<nav className="header-nav flex center align-center space-between">
+					<div className="logo-container">
+						<picture>
+							<img
+								src="../../OSM-assets/WordmarkBlack.svg"
+								alt="OneStaff logo."
+								className="header-logo"
+							/>
+						</picture>
+					</div>
+					<div className="top-menu-items flex center">
+						<ul className="flex">
+							<li>
+								<NavLink to="/traveljobs">Travel Jobs</NavLink>
+							</li>
+							<li>
+								<NavLink to="/benefits">Benefits</NavLink>
+							</li>
+							<li>
+								<NavLink to="/about">About</NavLink>
+							</li>
+							<li>
+								<NavLink to="/blogpage">Blog</NavLink>
+							</li>
+							<li>
+								<NavLink className="header-search-icon">{BsSearch}</NavLink>
+							</li>
+						</ul>
+					</div>
+					<div className="user-circle flex flex-end">
+						<FaUserCircle />
+					</div>
+				</nav>
+			</header>
 		</>
 	);
 }
