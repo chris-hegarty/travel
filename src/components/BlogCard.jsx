afd3267f06ebc8e0e473707ba004d59632e06fda
chegarty74 <chegarty74@yahoo.com>
2022-10-27 22:23:30 -0500

    Initial build of blog card

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { FaRegEye } from "react-icons/fa";
+import { FiStar } from "react-icons/fa";
 
 function BlogCard() {
 	return (
-		<div>
-			<h1>BlogCard</h1>
-		</div>
+		<article>
+			<img src="" alt="" />
+			<div>
+				<span>{FiStar}</span>
+				<span>{FiStar}</span>
+				<span>{FiStar}</span>
+				<span>{FiStar}</span>
+				<span>{FiStar}</span>
+				<span></span>
+			</div>
+			<h4>Blog Title</h4>
+			{/* views, comments here  */}
+			<div className="author-component">
+				<img src="" alt="" />
+				<div>
+					<h5>By Author</h5>
+					<h5>Date</h5>
+				</div>
+			</div>
+		</article>
 	);
 }
 
